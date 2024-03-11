be8f061307648c1a66f394e924b00617cbd16130
Sahil Rajesh Shriwardhankar <102847495+notsointresting@users.noreply.github.com>
2024-03-11 20:41:41 +0530

    Add files via upload

---
+++ chatbot_train.py
@@ -0,0 +1,18 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+
+def setup():
+    chatbot = ChatBot('Bot',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    trainer='chatterbot.trainers.ListTrainer')
+    trainer = ListTrainer(chatbot)
+    for file in os.listdir('../data/'):
+        convData = open(r'../data/' + file,encoding='latin-1').readlines()
+        #chatbot.set_trainer(ListTrainer)
+        #chatbot.train(convData)
+        trainer.train(convData)
+        #print("Training completed")
+    
+
+setup()
